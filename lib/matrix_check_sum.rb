5914f3d41fa3e3ba9206ce10398081e13cf260cf
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-29 17:58:25 -0700

    remove pry

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -1,4 +1,3 @@
-require 'pry'
 # Checks that for the given matrix, where number of rows are equal to number of columns
 # whether the sum of each row matches the sum of corresponding column i.e. sum
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
