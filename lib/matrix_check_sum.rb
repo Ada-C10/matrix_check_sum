d0c9b4297b8237d2ad0bc807fd30f212a415a0e9
kangazoom <michellelcronin@gmail.com>
2018-11-09 10:00:54 -0800

    brute forced it

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,5 +3,25 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+
+  length = matrix.length
+  counter = 0
+
+  while counter < length
+    row_sum = 0
+    column_sum = 0
+
+    length.times do |i|
+      row_sum += matrix[i][counter]
+      column_sum += matrix[counter][i]
+    end
+
+    if row_sum == column_sum
+      return true
+    end
+
+    counter += 1
+  end
+
+  return false
 end
