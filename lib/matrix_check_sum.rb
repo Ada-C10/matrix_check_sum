5ca5b1d170f591341b15a45d8d6e122f9fcfe219
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-15 18:10:49 -0800

    tests pass

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -2,6 +2,45 @@
 # whether the sum of each row matches the sum of corresponding column i.e. sum
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
+
+require 'pry'
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  i = 0
+  matrix_len = matrix.length #rows
+
+
+  # len = matrix_len.dup
+
+  matrix_len.times do
+
+    c_sum = 0
+    r_sum = 0
+    j = 0
+
+    row_len = matrix[i].length
+    #
+    # len2 = row_len.dup
+
+    row_len.times do
+
+
+      if matrix[j][i] != nil
+        c_sum += matrix[j][i]
+      end
+
+      if matrix[i][j] != nil
+        r_sum += matrix[i][j]
+      end
+
+      j+= 1
+    end
+
+    if c_sum != r_sum
+      return false
+    end
+
+    i += 1
+  end
+
+  return true
 end
