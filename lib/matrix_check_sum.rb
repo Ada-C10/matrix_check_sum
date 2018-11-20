849a658f09361da3538c537126afc7fa5887ecf2
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-11-20 11:32:13 -0800

    fib brute force

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,5 +3,12 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 def matrix_check_sum(matrix)
+  sum_boo = false
+  n = matrix[0].length
+
+  n.times do |i|
+    sum_row += n[i]
+  end
+
   raise NotImplementedError
 end
