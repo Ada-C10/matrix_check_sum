4a55f15a214b892dbb9256add09ace834b147224
Katie Jahanmir <katietj@gmail.com>
2018-11-26 18:42:14 -0800

    matrix sum complete

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,5 +3,20 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+  return false if (matrix.length) != (matrix[0].length)
+  (matrix.length).times do |i|
+    rows = 0
+    columns = 0
+    (matrix[0].length).times do |j|
+      rows += matrix[i][j]
+      columns += matrix[j][i]
+    end
+    if rows != columns
+      return false
+    end
 end
+return true
+end
+
+#time is O(n * m) where n and m are rows and columns
+#space is O(1)
